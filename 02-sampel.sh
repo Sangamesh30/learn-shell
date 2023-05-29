c5991074ccbdd7f4443dd60c8136be33f52532fc
Sangamesh30 <sangamesh.3094@gmail.com>
2023-05-29 14:53:58 +0530

    shell scripting testing

---
+++ 02-sampel.sh
@@ -0,0 +1,8 @@
+shell scripting color printing
+#print colors
+#echo -e "\e[COLmMESSAGE\e[0m"
+echo -e "\e[31m Hello world\e[0m"
+echo -e "\e[32m hello world\e[0m"
+echo -e "\e[33m Hello world\e[0m"
+echo -e "\e[34m Hello world\e[0m"
+echo -e "\e[35m Hello world\e[0m"
