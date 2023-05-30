cd2086cb507f1d20bf68db16e6828ca81d55dcd2
Sangamesh30 <sangamesh.3094@gmail.com>
2023-05-30 12:59:41 +0530

    roboshop frontend

--- 02-sampel.sh
+++ 02-sampel.sh
@@ -1,8 +1,8 @@
 shell scripting color printing
 #print colors
 #echo -e "\e[COLmMESSAGE\e[0m"
-echo -e "\e[31m Hello world\e[0m"
-echo -e "\e[32m hello world\e[0m"
-echo -e "\e[33m Hello world\e[0m"
-echo -e "\e[34m Hello world\e[0m"
-echo -e "\e[35m Hello world\e[0m"
+echo -e "\e[31m Hello world in red color\e[0m"
+echo -e "\e[32m hello world in green color\e[0m"
+echo -e "\e[33m Hello world in yello color\e[0m"
+echo -e "\e[34m Hello world in blue color\e[0m"
+echo -e "\e[35m Hello world in pink color\e[0m"
